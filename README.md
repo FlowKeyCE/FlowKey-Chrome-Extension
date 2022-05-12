bd9e3eff8047a5c49a2f8a41f1ea6acdfa3d41de
Chethiya Kusal <chethiyakdis@gmail.com>
2022-05-12 12:45:19 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Usage
+
+1. Install node modules, run
+   `yarn`
+
+2. Build extension, run
+   `yarn dev`
+
+3. Install the extension on Google Chrome
+4. Open a new tab an see the changes
---
+++ src/popup.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import { render } from "react-dom";
+
+function Popup() {
+  return <div className="container"></div>;
+}
+
+render(<Popup />, document.getElementById("react-target"));
