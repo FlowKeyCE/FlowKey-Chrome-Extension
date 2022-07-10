607de3b0dc50837158022fff4e6eca6b0e736ea9
Chethiya Kusal <chethiyakdis@gmail.com>
2022-07-10 14:38:29 +0530

    controllers

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from "react";
+import {
+  saveToStorage,
+  getFromStorage,
+} from "./controllers/storageController.js";
 import { render } from "react-dom";
 
 function Popup() {
