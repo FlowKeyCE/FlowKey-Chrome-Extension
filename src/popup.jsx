e9dbf0aac1a15938374e659786dae375998e41c8
Chethiya Kusal <chethiyakdis@gmail.com>
2024-12-03 19:25:45 +0530

    updated code

--- src/popup.jsx
+++ src/popup.jsx
@@ -3,10 +3,12 @@ import {
   saveToStorage,
   getFromStorage,
 } from "./controllers/storageController.js";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
+
 
 function Popup() {
   return <div className="container"></div>;
 }
 
-render(<Popup />, document.getElementById("react-target"));
+const root = createRoot(document.getElementById("react-target"));
+root.render(<Popup />);
