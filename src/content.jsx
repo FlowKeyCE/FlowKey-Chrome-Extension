60db5598c78a365ed91036ac3199d5d3c04eba4f
Chethiya Kusal <chethiyakdis@gmail.com>
2025-09-11 10:02:49 +0530

    shadowroot added

---
+++ src/content.jsx
@@ -0,0 +1,7 @@
+import { initPopup } from "./shadowRoot";
+
+// setInterval(() => {
+//   const popup = document.querySelector("#react-chrome-extension-popup");
+//   if (popup) return;
+//   initPopup();
+// }, 100);
